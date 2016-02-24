f8b9e84e307d823128418a98b5b7a872d4b81872
Palmer Harrell <palmerharrell@me.com>
2016-02-24 15:40:11 -0600

    Got setInterval and clearInterval to work

--- scripts/orchard.js
+++ scripts/orchard.js
@@ -1,11 +1,22 @@
 "use strict";
 
-function Plant() {
-	this.height = 0;
-};
+var pearDiv = document.getElementById("pearTree");
+var oakDiv = document.getElementById("oakTree");
+var pearInterval;
+var oakInterval;
+var counter = 0;
+
+function Plant() {};
+
+Plant.prototype.height = 0;
 
 Plant.prototype.increaseHeight = function(growth) {
-	this.height += growth;
+	for (var i = 0; i < growth; i++) {
+		this.height += 1;
+		if (this.height%10 === 0) {
+			this.branches++;
+		};
+	};	
 };
 
 Plant.prototype.decreaseHeight = function(growth) {
@@ -13,11 +24,11 @@ Plant.prototype.decreaseHeight = function(growth) {
 };
 
 
-function Tree() {
-	this.branches = 0;
-};
+function Tree() {};
 
 Tree.prototype = new Plant();
+
+Tree.prototype.branches = 0;
 
 Tree.prototype.grow = function(amount) {
 	this.increaseHeight(amount);
@@ -25,8 +36,40 @@ Tree.prototype.grow = function(amount) {
 
 Tree.prototype.trim = function(amount) {
 	this.decreaseHeight(amount);
+	this.branches -= 1;
 };
 
+var PearTree = new Tree();
+
+var OakTree = new Tree();
 
 
-// Steps 14 through 21
+pearInterval = setInterval(function () {
+  PearTree.grow(2);
+  pearDiv.innerHTML += `<p>Pear tree is now ${PearTree.height}cm tall and has ${PearTree.branches} branches.</p>`;
+}, 500);
+
+oakInterval = setInterval(function () {
+  OakTree.grow(3);
+  oakDiv.innerHTML += `<p>Oak tree is now ${OakTree.height}cm tall and has ${OakTree.branches} branches.</p>`;
+	counter++;
+	if (counter >= 30) {
+		console.log("finished");
+		clearInterval(pearInterval);
+		clearInterval(oakInterval);
+	};
+}, 500);
+
+
+// *** TO DO ***
+
+
+// Every tenth time the trees are grown, invoke the trim method. Pass one value to the method 
+// for the pear tree, and a larger value to the method on the oak tree.
+
+// The grow method does nothing but pass an argument to increaseHeight. Is that all it's supposed to do?
+
+// Change DOM output to be more like example
+
+
+

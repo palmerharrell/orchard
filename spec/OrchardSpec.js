ae142868713a4f257e0e6bdebd1dbd4006ffb53c
Palmer Harrell <palmerharrell@me.com>
2016-02-24 11:48:28 -0600

    Added toBeDefined unit tests

--- spec/OrchardSpec.js
+++ spec/OrchardSpec.js
@@ -1,27 +1,44 @@
-// Use Jasmine to write your unit tests before you begin writing any implementation code.
-
-// Unit Tests
-
-// There should be a Plant function defined.
-// There should be a Tree function defined.
-// There should be a function named increaseHeight on the prototype of Plant.
-// There should be a function named decreaseHeight on the prototype of Plant.
-// There should be a height property on Plant.
-// There should be a function named grow on the prototype of Tree.
-// There should be a function named trim on the prototype of Tree.
-// When you invoke the trim method on Tree, the height of the Tree should be reduced by the integer value that is passed as an argument (e.g. PearTree.trim(2))
-// When you invoke the trim method on Tree, the values of the branches property should be decreased by one.
-// When you invoke the grow method on Tree, the height of the Tree should be increased by the integer value that is passed as an argument (e.g. PearTree.grow(2))
-
-
 describe("The specification for Orchard", function() {
 
-  it("should have an add function", function() {
-    expect(add).toBeDefined();
+  it("should have a Plant function", function() {
+    expect(Plant).toBeDefined();
   });
 
-  it("should add two integers", function() {
-    expect(add(2, 5)).toBe(7);
+  it("should have a Tree function", function() {
+    expect(Tree).toBeDefined();
   });
+
+  it("should have an increaseHeight function on the prototype of Plant", function() {
+    expect(Plant.prototype.increaseHeight).toBeDefined();
+  });
+
+  it("should have a decreaseHeight function on the prototype of Plant", function() {
+    expect(Plant.prototype.decreaseHeight).toBeDefined();
+  });
+
+  it("should have a height property on Plant", function() {
+    expect(Plant.prototype.height).toBeDefined();
+  });
+
+  it("should have a grow function on the prototype of Tree", function() {
+    expect(Tree.prototype.grow).toBeDefined();
+  });
+
+  it("should have a trim function on the prototype of Tree", function() {
+    expect(Tree.prototype.trim).toBeDefined();
+  });
+
+  // When you invoke the trim method on Tree, the height of the Tree 
+	// should be reduced by the integer value that is passed as an argument (e.g. PearTree.trim(2))
+
+	// When you invoke the trim method on Tree, the values of the 
+	// branches property should be decreased by one.
+
+	// When you invoke the grow method on Tree, the height of the Tree 
+	// should be increased by the integer value that is passed as an argument (e.g. PearTree.grow(2))
+
+  // it("should add two integers", function() {
+  //   expect(add(2, 5)).toBe(7);
+  // });
 
 });
